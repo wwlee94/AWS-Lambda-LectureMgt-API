4b76e36fab878f4a433594db425bca583470500b
wwlee94 <38131683+wwlee94@users.noreply.github.com>
2019-10-21 13:12:29 +0900

    CSV 파일 DynamoDB로 import 하기

---
+++ CSV_Import_DynamoDB/lambda_function.py
@@ -0,0 +1,33 @@
+import json
+import csv
+import boto3
+
+def lambda_handler(event, context):
+    region= 'ap-northeast-2'
+    recList = []
+
+    s3 = boto3.client('s3')
+    dyndb = boto3.client('dynamodb', region_name= region)
+    confile = s3.get_object(Bucket='aws-lecture-bucket', Key='programmers_lecture.csv')
+    recList = confile['Body'].read().decode("utf-8-sig").split('\n')
+    csv_reader = csv.reader(recList, delimiter=',', quotechar='"')
+    for row in csv_reader:
+        if len(row) >= 1:
+            code = row[0]
+            lecture = row[1]
+            professor = row[2]
+            starttime = row[3]
+            endtime = row[4]
+            dayofweek = row[5]
+            response = dyndb.put_item(
+                TableName='programmers_lecture',
+                Item={
+                    'code' : {'S':code},
+                    'lecture':{'S':lecture},
+                    'professor':{'S':professor},
+                    'starttime':{'S':starttime},
+                    'endtime':{'S':endtime},
+                    'dayofweek':{'S':dayofweek}
+                }
+            )
+            print("put success")
